64444c851b26da9ca646dd8a202ce7b65ae74fe4
AltasinaArina <48995787+AltasinaArina@users.noreply.github.com>
2022-10-01 23:20:01 +0300

    Лаба2.3N

---
+++ Лаба2.3N.py
@@ -0,0 +1,18 @@
+import numpy as np
+from numpy import linalg as LA
+print("Возведение матрицы А размерностью 3х3 в степень -1")
+A = []
+for i in range(3):
+    A.append([])
+    for j in range(3):
+        print("Введите элемент матрицы с индексом ", i + 1, j + 1, ":", end='')
+        k = int(input())
+        A[i].append(k)
+a = np.array(A)
+print("Матрица А: ", a, sep='\n')
+o = np.linalg.det(a)
+if o == 0:
+    print("Матрица А в стемени -1 не существует, так как определитель матрицы А равен 0.")
+else:
+    b = np.linalg.inv(a)
+    print("Матрица А в степени -1: ", b, sep='\n')
